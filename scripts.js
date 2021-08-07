2ebf8a2a01f4d9f464c95097f8d92dd28c618c2e
Erenere <76115928+Erenere@users.noreply.github.com>
2021-08-07 16:58:09 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,102 @@
+/*let sources = ["https://images.hepsiburada.net/banners/s/0/672-378/bannerImage2106_20210531120758.png/format:webp",
+    "https://images.hepsiburada.net/banners/s/0/672-378/bannerImage2138_20210729151500.png/format:webp",
+    "https://images.hepsiburada.net/banners/s/0/672-378/bannerImage2087_20210527101722.png/format:webp",
+    "https://images.hepsiburada.net/banners/s/0/672-378/bannerImage2120_20210730172127.png/format:webp",
+    "https://images.hepsiburada.net/banners/s/0/672-378/bannerImage2088_20200911202947.png/format:webp"];*/
+//let backgroundColor = document.getElementsByClassName("slide");
+
+
+const nextButton = document.getElementById("next-button");
+const prevbutton = document.getElementById("prev-button");
+const slide = document.getElementsByClassName("slide");
+const miniButton =document.getElementsByClassName("mini-pic");
+let bc = document.getElementById("mobileContainer");
+const btnimg = document.querySelectorAll(".mini-pic img");
+
+let backgroundColors =["yellow","rgb(80,80,80)","orange","rgb(243, 225, 255)","purple",
+    "lightgreen","lightgray","gray","lightgray"];
+let count = 0;
+
+start();
+
+nextButton.addEventListener("click",()=>{
+    nextSlide();
+});
+
+prevbutton.addEventListener("click",()=>{
+    prevSlide();
+});
+
+for(let j=0;j<miniButton.length;j++){
+    miniButton[j].addEventListener("click",()=>{
+        for(let k=0; k<slide.length; k++){
+            slide[k].style.display = "none";
+        }
+        slide[j].style.display ="flex";
+        count = j;
+        changeBackground();
+        checkButton();
+    })
+}
+
+function nextSlide(){
+    if(count!==slide.length-1){
+        count++;
+        for(let i=0; i<slide.length;i++){
+            slide[i].style.display ="none";
+        }
+        slide[count].style.display ="flex";
+    }
+    changeBackground();
+    checkButton();
+}
+
+function prevSlide(){
+    if(count>0){
+        count--;
+        for(let i=0; i<slide.length;i++){
+            slide[i].style.display ="none";
+        }
+        slide[count].style.display ="flex";
+    }
+    changeBackground();
+    checkButton();
+
+}
+
+function start(){
+    for(let i=0; i<slide.length;i++){
+        slide[i].style.display ="none";
+    }
+    slide[0].style.display ="flex";
+    changeBackground();
+    checkButton();
+}
+
+function changeBackground(){
+    bc.style.backgroundColor = backgroundColors[count];
+    for(let h=0;h<slide.length;h++){
+        btnimg[h].style.border = "none";
+    }
+    btnimg[count].style.border = "2px solid white";
+}
+
+
+function checkButton(){
+    if(count===slide.length-1){
+        nextButton.classList.add("hidden");
+    }else {
+        nextButton.classList.remove("hidden");
+    }
+
+    if(count===0){
+        prevbutton.classList.add("hidden");
+    }else {
+        prevbutton.classList.remove("hidden");
+    }
+}
+
+setInterval(nextSlide,5000);
+
+
+
